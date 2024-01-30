3ec05aa633f606a94e43aefcb4143afb55d38b3b
Radhakrishnan Gopalakrishnan <radhakrishnangopalakrishnan@SFO-M-00097.lan>
2024-01-29 20:41:30 -0800

    feat : story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  //stories: [
+  //  '../stories/**/*.mdx',
+  // '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  // ],
+  stories: ['../components/templates/base/*.stories.@(js|jsx|mjs|ts|tsx)'],
   staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
